bd89fdf4921c5eb9de148461142e466c5b32edb1
Guido De Rosa <gderosa@umail.ucc.ie>
2022-03-12 01:10:59 +0000

    widest and tallest

--- x3p1.py
+++ x3p1.py
@@ -1,31 +1,70 @@
 # https://www.youtube.com/watch?v=094y1Z2wpJg
 
-import sys
-
 import matplotlib.pyplot as plt
-
-x = 0
-y = int(sys.argv[1])
-
-X = []
-Y = []
 
 def odd(n):
     return n % 2
 
-while y != 1:
+def next(y):
     if odd(y):
-        y = y*3 + 1
+        return y*3 + 1
     else:
-        y = y // 2
+        return y // 2
 
-    x += 1
 
-    # print(y)  # DEBUG
+MAX_Y_INIT      = 1e7
+
+max_x           = 0
+max_y           = 0
+
+widest_y_init   = 0
+tallest_y_init  = 0
+
+widest_X        = []
+widest_Y        = []
+tallest_X       = []
+tallest_Y       = []
+
+
+for y_init in range(1, int(MAX_Y_INIT)):
+    print(("%.1f %%" % (100*y_init/MAX_Y_INIT)), end="\r")
+
+    is_widest   = False
+    is_tallest  = False
+
+    x = 0
+    y = y_init
+
+    X = []
+    Y = []
+
+    while y > 1:
+        X.append(x)
+        Y.append(y)
+        x = x + 1
+        y = next(y)
+
+        if y >  max_y:
+            max_y = y
+            is_tallest = True
+
+    if x > max_x:
+        max_x = x
+        is_widest = True
 
     X.append(x)
     Y.append(y)
 
-plt.plot(X, Y)
-plt.show()
+    if is_widest:
+        widest_y_init   = y_init
+        widest_X        = X
+        widest_Y        = Y
+    if is_tallest:
+        tallest_y_init  = y_init
+        tallest_X       = X
+        tallest_Y       = Y
 
+plt.plot( widest_X,  widest_Y, label=f"\"Widest\" y_init={ widest_y_init}")
+plt.plot(tallest_X, tallest_Y, label=f"\"Tallest\" y_init={tallest_y_init}")
+plt.legend()
+plt.show()
