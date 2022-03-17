367446040d80543ed7c6270b8163089549e513ba
Guido De Rosa <gderosa@umail.ucc.ie>
2022-03-17 21:03:00 +0000

    do not overlog

--- x3p1-progr.py
+++ x3p1-progr.py
@@ -102,8 +102,8 @@ def main():
         y_0 = min(widest[0], tallest[0])
 
     while True:
-        print('%.1f M' % (y_0/1e6), end='\r')
-
+        if not y_0 % 1e6:
+            print('%.1f M' % (y_0/1e6), end='\r')
         found = False
         Y = sequence(y_0)
         y_max = max(Y)
